c2d7c305c62a7ed231af344b13521f376be39425
Kirill Temnenkov <kirill@temnenkov.com>
2021-09-16 23:14:57 +0300

    SudokuBoardOnePass

--- src/SudokuBoardOnePass.java
+++ src/SudokuBoardOnePass.java
@@ -1,23 +1,10 @@
+/*
+https://leetcode.com/problems/valid-sudoku/
+Runtime: 3 ms, faster than 49.87% of Java online submissions for Valid Sudoku.
+Memory Usage: 42.8 MB, less than 18.92% of Java online submissions for Valid Sudoku.
+ */
 class SudokuBoardOnePass {
 
-    public static void main(String[] args) {
-
-        char[][] board = new char[][] {
-                {'5','3','.','.','7','.','.','.','.'},
-                {'6','.','.','1','9','5','.','.','.'},
-                {'.','9','8','.','.','.','.','6','.'},
-                {'8','.','.','.','6','.','.','.','3'},
-                {'4','.','.','8','.','3','.','.','1'},
-                {'7','.','.','.','2','.','.','.','6'},
-                {'.','6','.','.','.','.','2','8','.'},
-                {'.','.','.','4','1','9','.','.','5'},
-                {'.','.','.','.','8','.','.','7','9'}
-        };
-
-        boolean res = new SudokuBoardOnePass().isValidSudoku(board);
-
-        System.out.println(res);
-    }
     private static final int[] DICT = {
             1, // '0'
             2, // '1'
@@ -39,12 +26,37 @@ class SudokuBoardOnePass {
         int[] checkerI = new int[N];
         int[] checkerJ = new int[N];
         int[][] checkerZ = new int[CELL_LEN][CELL_LEN];
+        int prevChecker;
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
 
+                int index = board[i][j] - RECODER;
+                if (index < 0) {
+                    continue;
+                }
+                int digit = DICT[index];
 
+                prevChecker = checkerI[i];
+                checkerI[i] = checkerI[i] | digit;
+                if (prevChecker == checkerI[i]) {
+                    return false;
+                }
 
+                prevChecker = checkerJ[j];
+                checkerJ[j] = checkerJ[j] | digit;
+                if (prevChecker == checkerJ[j]) {
+                    return false;
+                }
+
+                int zonedI = i / CELL_LEN;
+                int zonedJ = j / CELL_LEN;
+
+                prevChecker = checkerZ[zonedI][zonedJ];
+                checkerZ[zonedI][zonedJ] = checkerZ[zonedI][zonedJ] | digit;
+                if (prevChecker == checkerZ[zonedI][zonedJ]) {
+                    return false;
+                }
             }
         }
 
